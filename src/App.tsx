1ff2184e7115a528c57ed0aee63da32c987b6a67
Juan Maldini <juan.maldini@outlook.com>
2024-10-05 01:11:18 +0200

    meal-grid-recipes-ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,45 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import Header from "./components/Header";
 import SideNav from "./components/SideNav";
 import MainContent from "./components/MainContent";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
+import { Category, Meal } from "./types";
+import useHttpData from "./hooks/useHttpData";
+
+const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
+
+const makeMealUrl = (category: Category) =>
+  `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`;
+
+const defaultCategory = {
+  strCategory: "Beef",
+}
 
 function App() {
-  const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  console.log(data);
-  useEffect(() => {
-    const controller = new AbortController();
-    const { signal } = controller;
-    setLoading(true);
-
-    axios
-      .get(url, { signal })
-      .then(({ data }) => setData(data.meals))
-      .finally(() => setLoading(false));
-
-    return () => controller.abort();
-  }, []);
+  const [selectedCategory, setSelectedCategory] = useState<Category>(defaultCategory);
+  const{loading,data} = useHttpData<Category>(url);
+  const{loading: loadingMeal, data: dataMeal} = useHttpData<Meal>(makeMealUrl(defaultCategory));
+  console.log({dataMeal});
   return (
     <Grid
       templateAreas={`"header header"
                   "nav main"`}
       gridTemplateRows={"60px 1fr"}
       gridTemplateColumns={{ sm: `0 1fr`, md: `250px 1fr` }}
+      fontSize={14}
     >
-      <GridItem pl="2" bg="orange.300" area={"header"}>
+      <GridItem p="4" bg="orange.300" area={"header"}>
         <Header />
       </GridItem>
-      <GridItem pl="2" bg="pink.300" area={"nav"} height="calc(100vh - 60px)">
-        <SideNav />
+      <GridItem p="5" area={"nav"} height="calc(100vh - 60px)">
+        <SideNav
+          categories={data}
+          loading={loading}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
       </GridItem>
-      <GridItem pl="2" bg="green.300" area={"main"}>
-        <MainContent />
+      <GridItem p="2" bg="gray.100" area={"main"}>
+        <MainContent loading={loadingMeal} meals={dataMeal}/>
       </GridItem>
     </Grid>
   );
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Meal } from "../types";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 
-type Props = {}
+type Props = {
+  meals: Meal[];
+  loading: boolean;
+};
 
-function MainContent({}: Props) {
+function MainContent({ meals, loading }: Props) {
+  console.log(meals, loading);
   return (
-    <div>MainContent</div>
-  )
+    // Passing `columns={[2, null, 3]}` and `columns={{sm: 2, md: 3}}`
+    // will have the same effect.
+
+    <SimpleGrid columns={[2, null, 3]} spacing="40px">
+      {meals.map(m=>(
+        <Card ket={m.idMeal} keyboxShadow="lg">
+        <CardBody>
+          <Image
+            src={m.strMealThumb}
+            alt={m.strMeal}
+            borderRadius="lg"
+          />
+          <Heading size="md" color="blue.400">
+            <Text mt="4">{m.strMeal}</Text>
+          </Heading>
+        </CardBody>
+        <CardFooter pt="0">
+          <Button color="white" bgColor="blue.400">
+            Ver Receta
+          </Button>
+        </CardFooter>
+      </Card>
+      ))}
+    </SimpleGrid>
+  );
 }
 
-export default MainContent
+export default MainContent;
